886379651a90e581a037d87dee30cdd5877ad8c5
Sreelekha <117349474+Sreelekha-Sree@users.noreply.github.com>
2023-01-06 22:14:52 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import './index.css'
+const SocialButtons = () => 
+    (
+    <div className="bg-container">
+    <div className="main-container">
+        <h1 className="head" >Social Buttons</h1>
+      <div className="btn-container">
+        <button className="btn1">Like</button>
+        <button className="btn2">Comment</button>
+        <button className="btn3">Share</button>
+      </div>
+     </div>
+    </div>
+    )
+  export default  SocialButtons
+  
